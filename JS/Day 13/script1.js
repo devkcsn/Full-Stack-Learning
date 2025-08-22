7cf8e3603e78bf34ff71819966b236cb1dc59d61
Kumar Chandra Sheo Narayan <kumarchandrasheonarayan@outlook.com>
2025-08-22 10:03:59 +0530

    Refactor code structure for improved readability and maintainability

--- JS/Day 13/script1.js
+++ JS/Day 13/script1.js
@@ -10,13 +10,13 @@
 // console.log(isEven(4));
 
 // Fixed version:
-// function isEven(num){
-//     if(num%2==0){  //should check if remainder is 0 for even numbers
-//         return true;
-//     }
-//     return false;
-// }
-// console.log(isEven(4)); // Should return true
+function isEven(num){
+    if(num%2==0){  //should check if remainder is 0 for even numbers
+        return true;
+    }
+    return false;
+}
+console.log(isEven(4)); // Should return true
 
 
 // Buggy version - Array index out of bounds:
@@ -28,12 +28,12 @@
 // }
 
 // Fixed version:
-// let arr=[1,2,38];
-// if(5 < arr.length){
-//     console.log(arr[5].toString());
-// } else {
-//     console.log("Index out of bounds");
-// }
+let arr=[1,2,38];
+if(5 < arr.length){
+    console.log(arr[5].toString());
+} else {
+    console.log("Index out of bounds");
+}
 
 // ----------------------------------
 
@@ -46,12 +46,12 @@
 // }
 
 // Fixed version:
-// let arr1=[1,2,38];
-// if(arr1[5] !== undefined){
-//     console.log(arr1[5].toString());
-// } else {
-//     console.log("undefined");
-// }
+let arr1=[1,2,38];
+if(arr1[5] !== undefined){
+    console.log(arr1[5].toString());
+} else {
+    console.log("undefined");
+}
 
 let obj=null;
 if(obj !== null && obj.value !== null){
---
+++ JS/Day 14/Notes.md
@@ -0,0 +1,21 @@
+# Promises
+
+##### JS me Promises ek object hota hai jo future me asynchronous operation ke result ko represent karta hai.
+
+
+
+##### There are a total of 3 states
+
+##### 
+
+* ##### Pending
+* ##### fulfilled
+* ##### rejected
+
+In success part then keyword is used.
+In failure part catch keyword is use.
+And finally is always working, working on cleanup.
+
+Promises avoid CallBack and promote chaining.
+CallBack is also used for async JS
+
