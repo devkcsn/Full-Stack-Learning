1518cd0328b27a89c2978a6f1ba66964e01a361f
Kumar Chandra Sheo Narayan <kumarchandrasheonarayan@outlook.com>
2025-11-03 10:40:21 +0530

    Delete Learning Projects directory

--- Learning Projects/Todo list/script.js
+++
@@ -1,7 +0,0 @@
-let addBtn = document.querySelector(".add-btn");
-let model = document.querySelector(".model");
-
-addBtn.addEventListener("click", function () {
-    model.style.display = "block";
-});
-
